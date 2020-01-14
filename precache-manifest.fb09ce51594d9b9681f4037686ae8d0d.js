d2f061e42291f8dde3456978220a1471b66c0cd6
Ivan <i.romanov@protonmail.com>
2020-01-14 15:01:03 +0300

    Updates

---
+++ precache-manifest.fb09ce51594d9b9681f4037686ae8d0d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75988ad79d594a6b45290485a4d95276",
+    "url": "/web-creator/index.html"
+  },
+  {
+    "revision": "8737ab80d7ca5fe43c4a",
+    "url": "/web-creator/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "56b80e55c9f777db2ace",
+    "url": "/web-creator/static/css/main.68c12053.chunk.css"
+  },
+  {
+    "revision": "8737ab80d7ca5fe43c4a",
+    "url": "/web-creator/static/js/2.12b29e9c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/web-creator/static/js/2.12b29e9c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56b80e55c9f777db2ace",
+    "url": "/web-creator/static/js/main.05942708.chunk.js"
+  },
+  {
+    "revision": "fa0c29fc3243e870cddc",
+    "url": "/web-creator/static/js/runtime-main.15e798a2.js"
+  },
+  {
+    "revision": "19a81a31a8b549fcd16cba991c6c2e8f",
+    "url": "/web-creator/static/media/Montserrat-Regular.19a81a31.woff2"
+  },
+  {
+    "revision": "c0bfdcd26b201fcb6f8816ca77647fd5",
+    "url": "/web-creator/static/media/Montserrat-Regular.c0bfdcd2.woff"
+  },
+  {
+    "revision": "350f79f75959112c709596dfb4193a00",
+    "url": "/web-creator/static/media/Montserrat-SemiBold.350f79f7.woff2"
+  },
+  {
+    "revision": "67af366025f386d07c580f22f532f067",
+    "url": "/web-creator/static/media/Montserrat-SemiBold.67af3660.woff"
+  }
+]);
